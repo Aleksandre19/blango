147c47c000f44888aa05a12578014a123bdb89dc
Aleksandre  Aleksandre <aleksandre.voronini@gmail.com>
2023-04-11 15:21:55 +0000

    Finish security ad passwords

--- blango/settings.py
+++ blango/settings.py
@@ -54,6 +54,13 @@ class Dev(Configuration):
         },
     }
 
+    PASSWORD_HASHERS = [
+        'django.contrib.auth.hashers.Argon2PasswordHasher',
+        'django.contrib.auth.hashers.PBKDF2PasswordHasher',
+        'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
+        'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
+    ]
+
     X_FRAME_OPTIONS = 'ALLOW-FROM ' + os.environ.get('CODIO_HOSTNAME') + '-8000.codio.io'
     CSRF_COOKIE_SAMESITE = None
     CSRF_TRUSTED_ORIGINS = ['https://' + os.environ.get('CODIO_HOSTNAME') + '-8000.codio.io']
