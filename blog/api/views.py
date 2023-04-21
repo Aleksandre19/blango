17e5098c61c6083e91be0d352640b9227a8c596c
Aleksandre  Aleksandre <aleksandre.voronini@gmail.com>
2023-04-21 23:03:54 +0000

    Finish permissions

--- blog/api/views.py
+++ blog/api/views.py
@@ -3,10 +3,13 @@ from rest_framework import generics
 from blog.api.serializers import PostSerializer
 from blog.models import Post
 
+from blog.api.permissions import AuthorModifyOrReadOnly, IsAdminUserForObject
+
 class PostLists(generics.ListCreateAPIView):
   queryset = Post.objects.all()
   serializer_class = PostSerializer
 
 class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+  permission_calsses = [AuthorModifyOrReadOnly | IsAdminUserForObject]
   queryset = Post.objects.all()
   serializer_class = PostSerializer
