4c41c449f6bada54e8da8956ceaf21afc35ff6f2
Aleksandre  Aleksandre <aleksandre.voronini@gmail.com>
2023-05-05 16:32:24 +0000

    Finish JavaScript promises

--- blog/static/blog/blog.js
+++ blog/static/blog/blog.js
@@ -67,54 +67,84 @@
 
 // console.log(doubled)
 
+//=========== Clases
 
-class Greeter {
-  constructor (name) {
-    this.name = name
-  }
+// class Greeter {
+//   constructor (name) {
+//     this.name = name
+//   }
 
-  getGreeting () {
-    if (this.name === undefined) {
-      return 'Hello, no name'
-    }
+//   getGreeting () {
+//     if (this.name === undefined) {
+//       return 'Hello, no name'
+//     }
 
-    return 'Hello, ' + this.name
-  }
+//     return 'Hello, ' + this.name
+//   }
 
-  showGreeting (greetingMessage) {
-    console.log(greetingMessage)
-  }
+//   showGreeting (greetingMessage) {
+//     console.log(greetingMessage)
+//   }
 
-  greet () {
-    this.showGreeting(this.getGreeting())
-  }
+//   greet () {
+//     this.showGreeting(this.getGreeting())
+//   }
+// }
+
+// const g = new Greeter('Patchy')  // Put your name here if you like
+// g.greet()
+
+
+// class DelayedGreeter extends Greeter {
+//   delay = 2000
+
+//   constructor (name, delay) {
+//     super(name)
+//     if (delay !== undefined) {
+//       this.delay = delay
+//     }
+//   }
+
+//   greet () {
+//     setTimeout(
+//       () => {
+//         this.showGreeting(this.getGreeting())
+//       }, this.delay
+//     )
+//   }
+// }
+
+// const dg2 = new DelayedGreeter('Patchy 2 Seconds')
+// dg2.greet()
+
+// const dg1 = new DelayedGreeter('Patchy 1 Second', 1000)
+// dg1.greet()
+
+
+// ============== Promise
+
+
+function resolveCallback(data) {
+  console.log('Resolved with data' + data)
 }
 
-const g = new Greeter('Patchy')  // Put your name here if you like
-g.greet()
-
-
-class DelayedGreeter extends Greeter {
-  delay = 2000
-
-  constructor (name, delay) {
-    super(name)
-    if (delay !== undefined) {
-      this.delay = delay
-    }
-  }
-
-  greet () {
-    setTimeout(
-      () => {
-        this.showGreeting(this.getGreeting())
-      }, this.delay
-    )
-  }
+function rejectCallback(message) {
+  console.log('Reject with message' + message)
 }
 
-const dg2 = new DelayedGreeter('Patchy 2 Seconds')
-dg2.greet()
+const lazyAdd = function(a, b){
+  const doAdd = (resolve, reject) => {
+    if (typeof a !== 'number' || typeof b != 'number'){
+      reject("a and be must both be numbers")
+    }else{
+      const sum = a + b
+      resolve(sum)
+    }
+  }
+  return new Promise(doAdd)
+}
 
-const dg1 = new DelayedGreeter('Patchy 1 Second', 1000)
-dg1.greet()
+const p = lazyAdd(3, 4)
+p.then(resolveCallback, rejectCallback)
+
+lazyAdd('man', 'alsona').then(resolveCallback, rejectCallback)
