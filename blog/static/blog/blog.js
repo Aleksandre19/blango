16f538cedcb42ecf922b873f7598cfa34a184588
Aleksandre  Aleksandre <aleksandre.voronini@gmail.com>
2023-05-05 18:07:34 +0000

    Finish ReactJS

--- blog/static/blog/blog.js
+++ blog/static/blog/blog.js
@@ -1,3 +1,51 @@
+class ClickButton extends React.Component {
+  state = {
+    wasClicked: false
+  }
+
+  handleClick() {
+    this.setState(
+      {wasClicked: true}
+    )
+  }
+
+  render () {
+    let buttonText
+
+    if(this.state.wasClicked)
+      buttonText = 'Clicked!'
+    else
+      buttonText = 'Click Me'
+    
+    return React.createElement(
+      'button',
+      {
+        className: 'btn btn-primary mt-2',
+        onClick: () => {
+          this.handleClick()
+        }
+      },
+      buttonText
+    )
+  }
+}
+
+const domContainer = document.getElementById('react_root')
+ReactDOM.render(
+  React.createElement(ClickButton),
+  domContainer
+)
+
+
+
+
+
+
+
+
+
+
+// ================ Examples
 // const theNumber = 1
 // let yourName = 'Ben'
 
@@ -124,27 +172,27 @@
 // ============== Promise
 
 
-function resolveCallback(data) {
-  console.log('Resolved with data' + data)
-}
+// function resolveCallback(data) {
+//   console.log('Resolved with data' + data)
+// }
 
-function rejectCallback(message) {
-  console.log('Reject with message' + message)
-}
+// function rejectCallback(message) {
+//   console.log('Reject with message' + message)
+// }
 
-const lazyAdd = function(a, b){
-  const doAdd = (resolve, reject) => {
-    if (typeof a !== 'number' || typeof b != 'number'){
-      reject("a and be must both be numbers")
-    }else{
-      const sum = a + b
-      resolve(sum)
-    }
-  }
-  return new Promise(doAdd)
-}
+// const lazyAdd = function(a, b){
+//   const doAdd = (resolve, reject) => {
+//     if (typeof a !== 'number' || typeof b != 'number'){
+//       reject("a and be must both be numbers")
+//     }else{
+//       const sum = a + b
+//       resolve(sum)
+//     }
+//   }
+//   return new Promise(doAdd)
+// }
 
-const p = lazyAdd(3, 4)
-p.then(resolveCallback, rejectCallback)
+// const p = lazyAdd(3, 4)
+// p.then(resolveCallback, rejectCallback)
 
-lazyAdd('man', 'alsona').then(resolveCallback, rejectCallback)
+// lazyAdd('man', 'alsona').then(resolveCallback, rejectCallback)
